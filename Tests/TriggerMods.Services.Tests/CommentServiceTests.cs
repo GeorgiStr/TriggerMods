9e69629df8b95569f7f65c8ee9c9e893ebd696d2
GeorgiStr <georgi.str@mail.bg>
2019-08-30 22:35:35 +0300

    Added PrivateMessageService and CommentService Tests

---
+++ Tests/TriggerMods.Services.Tests/CommentServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TriggerMods.Data;
+using TriggerMods.Data.Models;
+using Xunit;
+
+namespace TriggerMods.Services.Tests
+{
+    public class CommentServiceTests
+    {
+        [Fact]
+        public void GetCommentsByUserNameShouldReturnAllComments()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                  .UseInMemoryDatabase(databaseName: "GetCommentsByUserNameShouldReturnAllComments_DB")
+                  .Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            var commentService = new CommentService(dbContext);
+
+            var user = new ApplicationUser
+            {
+                UserName = "Gosho",
+            };
+
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+
+            var c1 = new Comment
+            {
+                UserId = user.Id,
+                Content = "qweqweqwe",
+            };
+
+            var c2 = new Comment
+            {
+                UserId = user.Id,
+                Content = "asdasdasd",
+            };
+
+            var c3 = new Comment
+            {
+                UserId = user.Id,
+                Content = "zxczxczxc",
+            };
+
+            dbContext.Comments.AddRange(c1,c2,c3);
+            dbContext.SaveChanges();
+
+            var comments = commentService.GetCommentsByUserName(user.UserName).ToList();
+
+            Assert.Equal(3, comments.Count);
+        }
+    }
+}
---
+++ Tests/TriggerMods.Services.Tests/PrivateMessageServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TriggerMods.Data;
+using TriggerMods.Data.Models;
+using Xunit;
+
+namespace TriggerMods.Services.Tests
+{
+    public class PrivateMessageServiceTests
+    {
+        [Fact]
+        public void CreateShouldCreateVote()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                  .UseInMemoryDatabase(databaseName: "CreateShouldCreateVote_DB")
+                  .Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            var privateMessageService = new PrivateMessageService(dbContext);
+
+            var pm = new PrivateMessage();
+
+            privateMessageService.Create(pm);
+
+            var pmTest = dbContext.PrivateMessages.ToList();
+
+            Assert.Single(pmTest);
+        }
+    }
+}
